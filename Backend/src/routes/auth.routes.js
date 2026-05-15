0ef98b0112563ee4b412683ca5115d4549a3d5e1
Raees Aziz <100497350+raees-aziz@users.noreply.github.com>
2026-05-15 12:09:32 +0500

    Refactor auth routes to use router instance

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,24 +1,3 @@
-// import express from "express";
-// import {
-//   signup,
-//   login,
-//   logout,
-//   updateProfile,
-//   checkAuth,
-// } from "../controller/auth.controller.js";
-// import { protectRoute } from "../middleware/auth.middleware.js";
-
-// const router = express.Router();
-
-// router.post("/signup", signup);
-// router.post("/login", login);
-// router.post("/logout", logout);
-
-// router.put("/update-profile", protectRoute, updateProfile);
-// router.get("/check", protectRoute, checkAuth);
-
-// export default router;
-
 import express from "express";
 import {
   signup,
@@ -31,11 +10,32 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-express.Router().post("/signup", signup);
-express.Router().post("/login", login);
-express.Router().post("/logout", logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-express.Router().put("/update-profile", protectRoute, updateProfile);
-express.Router().get("/check", protectRoute, checkAuth);
+router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
+
+// import express from "express";
+// import {
+//   signup,
+//   login,
+//   logout,
+//   updateProfile,
+//   checkAuth,
+// } from "../controller/auth.controller.js";
+// import { protectRoute } from "../middleware/auth.middleware.js";
+
+// const router = express.Router();
+
+// express.Router().post("/signup", signup);
+// express.Router().post("/login", login);
+// express.Router().post("/logout", logout);
+
+// express.Router().put("/update-profile", protectRoute, updateProfile);
+// express.Router().get("/check", protectRoute, checkAuth);
+
+// export default router;
