50031fd6dd9945a1e39f083463987cd9a21a0c6d
Raees Aziz <100497350+raees-aziz@users.noreply.github.com>
2026-05-15 12:08:53 +0500

    Fixing the routes error

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,3 +1,24 @@
+// import express from "express";
+// import {
+//   signup,
+//   login,
+//   logout,
+//   updateProfile,
+//   checkAuth,
+// } from "../controller/auth.controller.js";
+// import { protectRoute } from "../middleware/auth.middleware.js";
+
+// const router = express.Router();
+
+// router.post("/signup", signup);
+// router.post("/login", login);
+// router.post("/logout", logout);
+
+// router.put("/update-profile", protectRoute, updateProfile);
+// router.get("/check", protectRoute, checkAuth);
+
+// export default router;
+
 import express from "express";
 import {
   signup,
@@ -10,11 +31,11 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
+express.Router().post("/signup", signup);
+express.Router().post("/login", login);
+express.Router().post("/logout", logout);
 
-router.put("/update-profile", protectRoute, updateProfile);
-router.get("/check", protectRoute, checkAuth);
+express.Router().put("/update-profile", protectRoute, updateProfile);
+express.Router().get("/check", protectRoute, checkAuth);
 
 export default router;
